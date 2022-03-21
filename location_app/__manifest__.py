e367ff59b24dd488bcf0498897264326e45e6164
toutchykipik <90046267+captivea-add@users.noreply.github.com>
2022-03-21 21:47:52 +0100

    create module

---
+++ location_app/__manifest__.py
@@ -0,0 +1,9 @@
+
+
+{
+'name': ' Gestion de Location bureau et de salles ',
+'description': ' Gérer une agence de location de Bureaux et de salles ',
+'author': 'BENHAMIDA Mustapha',
+'depends': ['base'],
+'application': True,
+}
